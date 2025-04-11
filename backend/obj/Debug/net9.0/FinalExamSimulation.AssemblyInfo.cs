072e7d2cf0bef308fc880271fff15a411615c90a
Ariel Bendahan <ariellielrey@hotmail.com>
2025-04-11 19:07:44 -0400

    Docker files

--- backend/obj/Debug/net9.0/FinalExamSimulation.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/FinalExamSimulation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalExamSimulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575c827d8ae6bd36464d7c8d67dab8240041fa14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce2d0ba014f495bc3b900778b173d38bc05c801")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalExamSimulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalExamSimulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
